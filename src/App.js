0a965616e18fb92644d22b16849acc8f098ecf81
Rimon Ahmed <rimonians@gmail.com>
2022-05-08 19:38:24 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div>
       <h3>React Formik</h3>
+      <p>Let's move with react formik</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
